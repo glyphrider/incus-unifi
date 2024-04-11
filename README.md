15f0889c96387a13e0c38a529ba807cd6e87d81b
Brian H. Ward <glyphrider@gmail.com>
2024-04-11 10:40:21 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# README
+
+Following (mostly) the instructions at help.ui.com, the setup-unifi.sh script will create a new incus container and
+install unifi and mongodb.
+
+`http://<ip>:8080` to access the console.
+
+I was able to successfully upload a backup from my unifi _app_ in TrueNAS SCALE.
---
+++ setup-unifi.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set +x
+
+incus launch images:debian/12 unifi
+incus exec unifi -- apt-get update
+incus exec unifi -- apt-get -y install ca-certificates apt-transport-https gnupg
+
+echo 'deb [ arch=amd64,arm64 ] https://www.ui.com/downloads/unifi/debian stable ubiquiti' > 100-ubnt-unifi.list
+incus file push ./100-ubnt-unifi.list unifi/etc/apt/sources.list.d/100-ubnt-unifi.list
+curl -fsSL https://dl.ui.com/unifi/unifi-repo.gpg > unifi-repo.gpg
+incus file push ./unifi-repo.gpg unifi/etc/apt/trusted.gpg.d/unifi-repo.gpg
+
+echo 'deb [ trusted=yes ] https://repo.mongodb.org/apt/debian bookworm/mongodb-org/7.0 main' > 100-mongodb-org.list
+incus file push ./100-mongodb-org.list unifi/etc/apt/sources.list.d/100-mongodb-org.list -pv
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | gpg --dearmor > mongodb-org.gpg
+incus file push ./mongodb-org.gpg unifi/etc/apt/trusted.gpg.d/mongodb-org.gpg -pv
+
+incus exec unifi -- apt-get update
+incus exec unifi -- apt-get -y install unifi
